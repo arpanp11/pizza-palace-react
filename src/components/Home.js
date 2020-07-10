127a3ad66b3070f65a2d62023bb0064c84432ae5
arpanp11 <arpanpatel2810@gmail.com>
2020-07-10 12:40:30 -0400

    add framer motion package

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const Home = () => {
   return (
-    <div className="home container">
-      <h2>
-        Welcome to Pizza Joint
-      </h2>
-      <Link to="/base">
-        <button>
-          Create Your Pizza
-        </button>
+    <motion.div
+      className='home container'
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+    >
+      <h2>Welcome to Pizza Palace</h2>
+      <Link to='/base'>
+        <motion.button animate={{}}>Create Your Pizza</motion.button>
       </Link>
-    </div>
-  )
-}
+    </motion.div>
+  );
+};
 
 export default Home;
