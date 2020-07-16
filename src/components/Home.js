426ab8ae2e4e661ffdad2b79ccbfc2fa6b99f4f2
arpanp11 <arpanpatel2810@gmail.com>
2020-07-16 13:54:55 -0400

    add animation effect on route

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,13 +14,33 @@ const buttonVariants = {
   },
 };
 
+const conatinerVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      delay: 1.5,
+      duration: 1.5,
+    },
+  },
+  exit: {
+    x: '-100vw',
+    transition: {
+      ease: 'easeInOut',
+    },
+  },
+};
+
 const Home = () => {
   return (
     <motion.div
       className='home container'
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ delay: 1.5, duration: 1.5 }}
+      variants={conatinerVariants}
+      initial='hidden'
+      animate='visible'
+      exit='exit'
     >
       <h2>Welcome to Pizza Palace</h2>
       <Link to='/base'>
